b6b49dcb76da2931b977da068474c01f5cb75103
SaiKumar Immadi <immadisaikumar@ymail.com>
2018-09-28 23:43:22 +0530

    starts with frontend index.js

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'jquery/dist/js/jquery.js';
+import 'bootstrap/dist/js/bootstrap.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
