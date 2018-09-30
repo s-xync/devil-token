12354add315e5e2f697d73e3bd759f80e302568c
SaiKumar Immadi <immadisaikumar@ymail.com>
2018-09-30 20:43:39 +0530

    makes the navbar fixed-top

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -5,18 +5,20 @@ class Navbar extends Component{
   render(){
     return(
       <React.Fragment>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div className="container">
-            <Link to="/"><div className="navbar-brand">DVTK</div></Link>
-            <div className="navbar-nav ml-md-auto">
-              <Link to="/wallet"><div className={"nav-item nav-link "+this.props.walletStatus}>Wallet</div></Link>
-              <Link to="/send"><div className={"nav-item nav-link "+this.props.sendStatus}>Send</div></Link>
+        <div>
+          <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+              <Link to="/"><div className="navbar-brand">DVTK</div></Link>
+              <div className="navbar-nav ml-md-auto">
+                <Link to="/wallet"><div className={"nav-item nav-link "+this.props.walletStatus}>Wallet</div></Link>
+                <Link to="/send"><div className={"nav-item nav-link "+this.props.sendStatus}>Send</div></Link>
+              </div>
             </div>
-          </div>
-        </nav>
-      </React.Fragment>
-    );
-  }//render ends
-}
+          </nav>
+        </div>
+          </React.Fragment>
+        );
+      }//render ends
+    }
 
-export default Navbar;
+    export default Navbar;
