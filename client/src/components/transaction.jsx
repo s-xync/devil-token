6c3a6605fa795402d115b9f1315ae4096599c0a1
SaiKumar Immadi <immadisaikumar@ymail.com>
2018-09-30 23:06:26 +0530

    table working well with divs than the table

--- client/src/components/transaction.jsx
+++ client/src/components/transaction.jsx
@@ -4,16 +4,17 @@ class Transaction extends Component{
   render(){
     return(
       <React.Fragment>
-        <tr>
-          <td>{this.props.transaction.timeString}</td>
-          <td>{this.props.transaction.fromAddress}</td>
-          <td>{this.props.transaction.toAddress}</td>
-          <td className={this.props.transaction.value<0?"table-danger":"table-success"}>{this.props.transaction.value}</td>
-          <td><a href={this.props.transaction.etherScanURL} className="btn btn-outline-primary btn-lg" role="button" aria-pressed="true" target="_blank" rel="noopener noreferrer">Details</a></td>
-        </tr>
+        <div className="row">
+          <div className="col-md-2">{this.props.transaction.timeString}</div>
+          <div className="col-md-3 overflow-text">{this.props.transaction.fromAddress}</div>
+          <div className="col-md-3 overflow-text">{this.props.transaction.toAddress}</div>
+          <div className={"col-md-2 "+(this.props.transaction.value < 0 ? "text-danger" : "text-success")}>{this.props.transaction.value+" "+this.props.tokenSymbol}</div>
+          <div className="col-md-2"><a href={this.props.transaction.etherScanURL} className="btn btn-outline-primary btn-sm" role="button" aria-pressed="true" target="_blank" rel="noopener noreferrer">Details</a></div>
+        </div>
+        <hr className="my-1"></hr>
       </React.Fragment>
     );
-  }
+  }//render ends
 }
 
 export default Transaction;
--- client/src/components/transactions.jsx
+++ client/src/components/transactions.jsx
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
-import Transaction from './transaction.jsx'
+import Transaction from './transaction.jsx';
 
 class TransactionsTable extends Component{
   render(){
     return(
       <React.Fragment>
-        <div className="jumbotron text-center" style={{width:'1140px',paddingTop:'16px',paddingBottom:'8px',margin:'0px'}}>
+        <div className="jumbotron text-center" style={{width:'1140px',paddingTop:'16px',paddingBottom:'16px'}}>
           <h4>Transactions</h4>
-          <table className="table table-hover table-bordered">
-            <thead className="thead-dark">
-              <tr>
-                <th scope="col">Time</th>
-                <th scope="col">From</th>
-                <th scope="col">To</th>
-                <th scope="col">Value</th>
-                <th scope="col">EtherScan</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.props.transactions.map((transaction)=><Transaction key={transaction.etherScanURL} transaction={transaction} />)}
-            </tbody>
-          </table>
+          <hr className="my-2"/>
+          <div className="row">
+            <div className="col-md-2"><h5><b>Time</b></h5></div>
+            <div className="col-md-3"><h5><b>From</b></h5></div>
+            <div className="col-md-3"><h5><b>To</b></h5></div>
+            <div className="col-md-2"><h5><b>Value</b></h5></div>
+            <div className="col-md-2"><h5><b>EtherScan</b></h5></div>
+          </div>
+          <hr className="my-2"/>
+          <div>
+            {this.props.transactions.map((transaction)=>
+              <Transaction key={transaction.etherScanURL} transaction={transaction} tokenSymbol={this.props.tokenSymbol} />
+            )}
+          </div>
         </div>
       </React.Fragment>
     );
-  }
+  }//render ends
 }
 
 export default TransactionsTable;
